4330b35ff3d06eaf2c6308fa49665d1e4c2226bb
lana havryluik <elizat06@ukr.net>
2020-11-27 13:28:26 +0200

    Add catalog

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'users-list';
+
+  animals: string[] = ['Dogs', 'Cats', 'Parrots', 'Hamsters', 'Turtles']
+  
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatListModule } from '@angular/material/list';
+import { UsersListComponent } from './users-list/users-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsersListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatListModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
